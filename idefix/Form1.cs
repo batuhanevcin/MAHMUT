4a33cb30ef565689c3041a90f567023d03971f1b
Hasan Emir Yıldırım <hasanyldrm@hotmail.de>
2019-01-08 03:27:38 -0800

    NewTonSoft.Json kuruldu (nuget üzerinden)

--- idefix/Form1.cs
+++ idefix/Form1.cs
@@ -16,5 +16,10 @@ namespace idefix
         {
             InitializeComponent();
         }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
